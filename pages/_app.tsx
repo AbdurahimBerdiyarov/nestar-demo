954aa4d6cb7d230ac36859a080a0d27131064dcd
AbdurahimBerdiyarov <hoorwen@icloud.com>
2024-06-05 20:44:17 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { light } from "../scss/MaterialTheme/index";
+import { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // @ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
   console.log("App COMPONENT - PAGES ROUTER");
 
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
